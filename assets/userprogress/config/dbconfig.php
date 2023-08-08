5f60997bdb65935c5b37885addf83f431809dbe4
sandhya <sandhyaramachandra24@gmail.com>
2023-08-08 19:25:47 +0530

    user progress added

---
+++ assets/userprogress/config/dbconfig.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$cfg_dbhost = 'localhost';
+$cfg_dbuser = 'root';
+$cfg_dbpassword = '';
+$cfg_database = 'abbchmprmdb1';
+
+/*$cfg_dbhost = 'localhost';
+$cfg_dbuser = 'root';
+$cfg_dbpassword = 'Calisto+1!';
+$cfg_database = 'abbchmprmdb';*/
+?>
